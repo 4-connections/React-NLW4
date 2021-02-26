3171dd154d2deb47058dfc2383b6079ae340454a
4-connections <4connections.solucoes@gmail.com>
2021-02-26 00:14:07 -0300

    subindo aplicação

---
+++ moveit/src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from './components/Button'
+
+// componente é uma função que retorna um jsx um "HTML" 
+function App() {
+  return (
+    <div>
+        <Button/>
+        <Button/>
+        <Button/>
+        <Button/>
+        <Button/>
+        <Button/>
+    </div>
+  );
+}
+
+export default App;
